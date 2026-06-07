7078197bc39b0c81d50e05c3aa8d1e202165493a
c4frf94rdv-crypto <c4frf94rdv@privaterelay.appleid.com>
2026-06-07 17:21:28 +0200

    fix: Remove version declaration from docker-compose.yml and enhance entrypoint script for terminal compatibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-tmux new-session -d -s garmin 'python main.py'
+tmux new-session -d -s garmin 'export TERM=xterm-256color && python main.py; exec bash -i'
 
 ttyd -p 9000 -W -t fontSize=16 tmux attach -t garmin
